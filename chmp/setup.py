caf9b88f19ab24565313f07b0d4fa2d2f3640bb8
CMP <c.prohm@googlemail.com>
2018-05-14 22:05:26 +0200

    Release first version

--- chmp/setup.py
+++ chmp/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chmp',
+    version='0.1.0',
     packages=[
         'chmp.{}'.format(package)
         for package in find_packages('src/chmp')
