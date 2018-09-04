8eb97e795c2ed42527b7face7f04ff5a319ffa78
CMP <c.prohm@googlemail.com>
2018-09-04 21:14:26 +0200

    Bump version

--- chmp/setup.py
+++ chmp/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, PEP420PackageFinder
 
 setup(
     name="chmp",
-    version="2018.9.0",
+    version="2018.9.1",
     packages=PEP420PackageFinder.find("src"),
     package_dir={"": "src"},
     tests_require=["pytest"],
