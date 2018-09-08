e5451c5bb71a33a37a389cc4cdb10cfd24509cb7
CMP <c.prohm@googlemail.com>
2018-09-08 09:46:25 +0200

    Switch chmp to setuptools_scm

--- chmp/setup.py
+++ chmp/setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, PEP420PackageFinder
 
 setup(
     name="chmp",
-    version="2018.9.1",
     packages=PEP420PackageFinder.find("src"),
     package_dir={"": "src"},
     tests_require=["pytest"],
+    use_scm_version={"root": "..", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
 )
