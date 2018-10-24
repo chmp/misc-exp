a2a985b53d345f0cf782bc5291c9ef5c6f7ac387
CMP <c.prohm@googlemail.com>
2018-10-24 22:57:21 +0200

    Make sure setup.py works correctly

--- 20181026-TestingInJupyter/setup.py
+++ 20181026-TestingInJupyter/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, PEP420PackageFinder
+from setuptools import setup
 
 
 setup(
     name='ipytest-demo',
     version='0.0.0',
-    packages=PEP420PackageFinder.find("src"),
+    py_modules=["keep_odds"],
     package_dir={'': 'src'},
 )
