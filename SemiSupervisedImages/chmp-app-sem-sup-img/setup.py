72f9cec759e4d61b81d53c1fea339a5a0503529e
CMP <c.prohm@googlemail.com>
2019-05-13 20:05:27 +0200

    Move hierarchical sampling code into Building Blocks

--- SemiSupervisedImages/chmp-app-sem-sup-img/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='chmp-app-sem-sup-img',
-    packages=["chmp.app.sem_sup_img"],
-    package_dir={'': 'src'},
-    install_requires=['Pillow', 'numpy', 'click']
-)
